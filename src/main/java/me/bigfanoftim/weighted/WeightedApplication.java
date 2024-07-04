351d45aad242a5ad124d535069d21e4511dbde19
bigfanoftim <bigfanoftim@gmail.com>
2024-07-04 17:59:01 +0900

    프로젝트 생성

---
+++ src/main/java/me/bigfanoftim/weighted/WeightedApplication.java
@@ -0,0 +1,12 @@
+package me.bigfanoftim.weighted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeightedApplication.class, args);
+    }
+}
---
+++ src/test/java/me/bigfanoftim/weighted/WeightedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bigfanoftim.weighted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
